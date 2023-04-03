48b45005700f5249cbdc3b880ab3cd55d35e509c
Stefan Thomsen <thomsenstefan@outlook.com>
2023-04-03 22:49:42 +0200

    Add project files.

---
+++ idpa_IoT-Abfahrtsbildschirm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace idpa_IoT_Abfahrtsbildschirm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
