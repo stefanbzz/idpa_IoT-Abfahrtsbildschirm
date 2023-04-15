a147b881f03162014979fd43c1ae17f266203503
Stefan Thomsen <78475321+stefanbzz@users.noreply.github.com>
2023-04-15 18:36:48 +0200

    Create model classes according to the API call values.

---
+++ idpa_IoT-Abfahrtsbildschirm/Models/Terminal.cs
@@ -0,0 +1,10 @@
+﻿namespace idpa_IoT_Abfahrtsbildschirm.Models
+{
+    public class Terminal
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
